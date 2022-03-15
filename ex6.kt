d43e91f8c136f06dfaa21c13d3eec30305260380
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-03-15 20:07:56 +0900

    Update ex6.kt

--- ex6.kt
+++ ex6.kt
@@ -16,7 +16,7 @@ class Calculator constructor() {
         }
     }
     fun sum(number1: Double, number2: Double): Double{
-        return number1 - number2
+        return number1 + number2
     }
     fun subtract(number1: Double, number2: Double): Double{
         return number1 - number2
